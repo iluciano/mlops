ec7c10e45f735d15625ba85a326087e8b386bb5b
Igor Luciano <igorlucianodev@gmail.com>
2023-06-10 18:37:38 -0300

    modelo

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+# Use uma imagem base Python
+FROM python:3.9-slim
+
+# Cria um diretório Base na imagem
+RUN mkdir -p /mlopsServer
+RUN mkdir -p /Log
+
+# Defina o diretório de trabalho dentro do contêiner
+WORKDIR /mlopsServer
+
+# Copie o arquivo requirements.txt para o contêiner
+COPY requirements.txt .
+
+# Instale as dependências usando o pip
+RUN pip install --upgrade pip
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copie todos os arquivos do diretório atual para o diretório de trabalho do contêiner
+COPY . /mlopsServer
+
+# Defina a porta em que o aplicativo estará em execução (substitua 8000 pela porta apropriada, se necessário)
+EXPOSE 8000
+EXPOSE 80
+EXPOSE 8080
+
+# Execute o comando para iniciar o aplicativo
+CMD ["uvicorn", "app:app", "--host", "0.0.0.0", "--port", "8000"]
---
+++ app.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI
+import pickle
+from pydantic import BaseModel
+
+# Definir a classe do modelo de entrada
+class InputData(BaseModel):
+    loan_amount: float
+    rate_of_interest: float
+    Interest_rate_spread: float
+    Upfront_charges: float
+    income: float
+
+# Carregar o modelo salvo
+with open("modelo.pkl", "rb") as file:
+    model = pickle.load(file)
+
+# Criar a instância do FastAPI
+app = FastAPI()
+
+# Definir uma rota para receber a entrada e fazer a previsão
+@app.post("/predict")
+def predict(input_data: InputData):
+    # Converter os dados de entrada em um array numpy
+    input_array = [[input_data.loan_amount, input_data.rate_of_interest, input_data.Interest_rate_spread, 
+                    input_data.Upfront_charges, input_data.income]]
+
+    # Fazer a previsão usando o modelo carregado
+    prediction = model.predict(input_array)
+    probability = model.predict_proba(input_array).max() * 100
+
+    # Retornar a previsão como resultado
+    #return {"prediction": prediction.tolist()}
+    if prediction == 1:
+        return {
+            "prediction": "Este cliente provavelmente irá atrasar o pagamento do empréstimo.",
+            "probability": f"Modelo preditivo com {probability:.4f}% de probabilidade"
+        }
+    else:
+        return {
+            "prediction": "Este cliente tem baixa propensão de atrasar o pagamento do empréstimo.",
+            "probability": f"Modelo preditivo com {probability:.4f}% de probabilidade"
+        }
+
+# Iniciar o servidor FastAPI usando o uvicorn
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8000)
